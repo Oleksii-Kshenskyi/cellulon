f431fc85b5c8ce8cc20f7562a5852f79853e57a7
Oleksii-Kshenskyi <oleksii.kshenskyi@outlook.com>
2026-03-26 04:25:04 +0200

    Randomly spawning camera as first line of setup makes window maximize work??? Bevy, WTF?

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,17 @@
 use bevy::{
-    diagnostic::{LogDiagnosticsPlugin},
     prelude::*,
     window::{
-        PresentMode, WindowMode, WindowResolution, WindowTheme
+        PresentMode, WindowMode, WindowTheme
     },
 };
 
-fn max_window(mut window: Query<&mut Window>) {
-    let mut window = window.single_mut().unwrap();
+fn max_window(mut commands: Commands, mut window: Single<&mut Window>) {
+    commands.spawn(Camera2d);
+    window.visible = true;
     window.set_maximized(true);
-    window.position = WindowPosition::Centered(MonitorSelection::Primary);
-    error!("WELL, I FREAKING TRIED {}", window.title);
 }
 
 fn main() {
-    unsafe {
-        std::env::set_var("WINIT_UNIX_BACKEND", "x11");
-    }
-    println!("Back? {}", std::env::var("WINIT_UNIX_BACKEND").unwrap_or_else(|_| "NOT SET".into()));
-
     App::new()
         .add_plugins((
             DefaultPlugins.set(WindowPlugin {
@@ -27,14 +20,12 @@ fn main() {
                     decorations: true,
                     mode: WindowMode::Windowed,
                     position: WindowPosition::Centered(MonitorSelection::Primary),
-                    resolution: WindowResolution::new(1280, 720).with_scale_factor_override(1.0),
-                    present_mode: PresentMode::Fifo,
+                    present_mode: PresentMode::AutoVsync,
                     window_theme: Some(WindowTheme::Dark),
                     ..default()
                 }),
                 ..default()
             }),
-            LogDiagnosticsPlugin::default(),
         ))
         .add_systems(Startup, max_window)
         .run();
